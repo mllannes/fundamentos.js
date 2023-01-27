b21ec493cb82fc3efda093f057f5ecb073c50e17
mllannes <mariilannes93@gmail.com>
2023-01-27 15:23:02 -0300

    exemplos aula 1

---
+++ Tipos primitivos.js
@@ -0,0 +1,20 @@
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = 'niteroi'
+const input = 'Niteroi'
+
+const inputMinusculo = input.toLowerCase()
+
+console.log(cidade === inputMinusculo)
+
+const senha = 'mariana0511'
+
+console.log(senha.length)
