87ef46cf1835ef4d7ec15738e11a6730ad0a2c39
mllannes <mariilannes93@gmail.com>
2023-01-30 16:53:53 -0300

    codigos e conversões

---
+++ funcoes.js
@@ -0,0 +1,23 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// 1 - Declara a função:
+
+function imprimeTexto(texto) {
+  console.log(texto)
+}
+
+// 2 - Executa a função (1 ou + vezes):
+
+imprimeTexto(soma())
+//imprimeTexto('outro texto')
+
+// 3 - Três formas de escrever funções:
+
+function soma() {
+  return 2 + 2
+}
+//console.log(soma())
